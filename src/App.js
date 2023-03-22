ed3baae6de4a384cafbd9a020a79176d357d02d7
charbs0701 <chaqhdtjr7@gmail.com>
2023-03-23 02:33:37 +0900

    Feat Header, Home margin

--- src/App.js
+++ src/App.js
@@ -3,17 +3,23 @@ import Home from "./pages/home/Home";
 import Setting from "./pages/setting/Setting";
 import Login from "./pages/login/Login";
 import Link from "./pages/link/Link";
+import Header from "./pages/home/Header";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/setting" element={<Setting />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/link" element={<Link />} />
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div style={{ margin: "3% 3%" }}>
+      <Router>
+        <Header />
+        <div style={{ margin: "0 6%" }}>
+          <Routes>
+            <Route path="/setting" element={<Setting />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/link" element={<Link />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
   );
 }
 
