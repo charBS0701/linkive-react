90623b85421875ba58200cfcd70fe9cec2869575
charbs0701 <chaqhdtjr7@gmail.com>
2023-06-06 16:09:17 +0900

    Fix Header ProfileImg 경로수정

--- src/pages/home/BtnProfile.jsx
+++ src/pages/home/BtnProfile.jsx
@@ -11,7 +11,7 @@ const BtnProfile = ({ isLoggedIn }) => {
       if (isLoggedIn) {
         try {
           const response = await axios.get(
-            `${process.env.REACT_APP_SERVER}/users/profileImg`,
+            `api/users/profileImg`,
             {
               withCredentials: true,
             }
